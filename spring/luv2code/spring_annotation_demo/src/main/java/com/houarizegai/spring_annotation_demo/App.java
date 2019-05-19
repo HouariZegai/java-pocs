49e1337f9063de975a132ece4c361dde39015df1
unknown <houarizegai14@gmail.com>
2019-05-19 14:48:33 +0100

    Spring - luv2code - Adding annotation project

---
+++ spring/luv2code/spring_annotation_demo/src/main/java/com/houarizegai/spring_annotation_demo/App.java
@@ -0,0 +1,11 @@
+package com.houarizegai.spring_annotation_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
