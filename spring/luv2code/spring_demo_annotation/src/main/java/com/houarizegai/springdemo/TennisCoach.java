8ca00e80dc85c850c2d3d9a9d7106ab4be01791d
unknown <houarizegai14@gmail.com>
2019-05-19 15:55:03 +0100

    Spring - luv2code - setter injection using autowired annotation

--- spring/luv2code/spring_demo_annotation/src/main/java/com/houarizegai/springdemo/TennisCoach.java
+++ spring/luv2code/spring_demo_annotation/src/main/java/com/houarizegai/springdemo/TennisCoach.java
@@ -9,17 +9,36 @@ public class TennisCoach implements Coach {
 
 	FortuneService fortuneService;
 	
-	@Autowired
+	public TennisCoach() {
+		
+	}
+	
+	/*	
+    @Autowired
 	public TennisCoach(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
 	}
+ 	*/
 	
 	public String getDailyWorkout() {
 		return "Practice your backhand volley";
 	}
 	
+	/*
+	@Autowired
+	public void setDailyFortune(FortuneService fortuneService) {
+		System.out.println("setDailyFortune() called !");
+		this.fortuneService = fortuneService;
+	}
+	*/
+	
+	@Autowired
+	public void doSomeCrazyStuff(FortuneService fortuneService) {
+		System.out.println("doSomeCrazyStuff() called !");
+		this.fortuneService = fortuneService;
+	}
+	
 	public String getDailyFortune() {
 		return fortuneService.getFortune();
 	}
-	
 }
