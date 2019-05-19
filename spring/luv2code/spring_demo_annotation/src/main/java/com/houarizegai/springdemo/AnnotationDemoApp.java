c828c5c0192d70e60cbc8140360eb69c2789e656
unknown <houarizegai14@gmail.com>
2019-05-19 18:43:43 +0100

    Spring - luv2code - PostContruct & PreDestroy annotation

--- spring/luv2code/spring_demo_annotation/src/main/java/com/houarizegai/springdemo/AnnotationDemoApp.java
+++ spring/luv2code/spring_demo_annotation/src/main/java/com/houarizegai/springdemo/AnnotationDemoApp.java
@@ -10,12 +10,17 @@ public class AnnotationDemoApp {
 		
 		// get the bean from the bean container
 		Coach myCoach = context.getBean("tennisCoach", Coach.class);
+		Coach alphaCoach = context.getBean("tennisCoach", Coach.class);
+		
+		System.out.println(myCoach == alphaCoach);
+		System.out.println(myCoach);
+		System.out.println(alphaCoach);
 		
 		// call a method on the bean
-		System.out.println(myCoach.getDailyWorkout());
+		//System.out.println(myCoach.getDailyWorkout());
 
 		// call method to get the daily fortune
-		System.out.println(myCoach.getDailyFortune());
+		//System.out.println(myCoach.getDailyFortune());
 		
 		// close the context
 		context.close();
--- spring/luv2code/spring_demo_annotation/src/main/java/com/houarizegai/springdemo/TennisCoach.java
+++ spring/luv2code/spring_demo_annotation/src/main/java/com/houarizegai/springdemo/TennisCoach.java
@@ -2,10 +2,14 @@ package com.houarizegai.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 //@Component("thatSillyCoach") // specify the id of bean
 @Component // default bean id: take the name of class, make the first character lower case.
+//@Scope("prototype")
 public class TennisCoach implements Coach {
 	// field injection using java technology called Reflection
 	@Autowired
@@ -42,7 +46,21 @@ public class TennisCoach implements Coach {
 		this.fortuneService = fortuneService;
 	}
 	*/
+	
 	public String getDailyFortune() {
 		return fortuneService.getFortune();
 	}
+	
+	// my init method
+	@PostConstruct
+	public void init() {
+		System.out.println(">> TennisCoach: inside init()");
+	}
+	
+	// my destroy method
+	@PreDestroy
+	public void destroy() {
+		System.out.println(">> TennisCoach: inside destroy()");
+	}
+	
 }
