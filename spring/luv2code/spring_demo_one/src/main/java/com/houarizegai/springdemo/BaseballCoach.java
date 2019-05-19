04d3dc81f1096e3592a6a63aa9c8f47ad7efe72c
unknown <houarizegai14@gmail.com>
2019-05-19 14:47:38 +0100

    Spring - luv2code - adding first learning codes (basic)

---
+++ spring/luv2code/spring_demo_one/src/main/java/com/houarizegai/springdemo/BaseballCoach.java
@@ -0,0 +1,32 @@
+package com.houarizegai.springdemo;
+
+public class BaseballCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	private String name;
+	
+	public BaseballCoach() {
+		
+	}
+	
+	public BaseballCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+	
+	public void setName(String name) {
+		System.out.println("setName called, value=" + name);
+		this.name = name;
+	}
+	
+	public FortuneService getFortuneService() {
+		return fortuneService;
+	}
+	
+	public void setFortuneService(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+	
+	public String getDailyWorkout() {
+		return "Spend 30 minutes on batting practice";
+	}
+}
---
+++ spring/luv2code/spring_demo_one/src/main/java/com/houarizegai/springdemo/FortuneService.java
@@ -0,0 +1,5 @@
+package com.houarizegai.springdemo;
+
+public class FortuneService {
+	
+}
