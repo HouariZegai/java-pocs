19a76a6752330d1a2b325d23eca319507371c4ad
unknown <houarizegai14@gmail.com>
2019-05-21 14:47:36 +0100

    Spring MVC - use RequestMapping in controller

--- spring/luv2code/spring-mvc-demo/WebContent/WEB-INF/view/main-menu.jsp
+++ spring/luv2code/spring-mvc-demo/WebContent/WEB-INF/view/main-menu.jsp
@@ -6,6 +6,6 @@
 </head>
 <body>
 	<h2>Spring MVC Demo - Home Page !</h2>
-	<a href="showForm">Show Student Form</a>
+	<a href="hello/showForm">Show Student Form</a>
 </body>
 </html>
--- spring/luv2code/spring-mvc-demo/src/com/houarizegai/springdemo/mvc/HelloWorldController.java
+++ spring/luv2code/spring-mvc-demo/src/com/houarizegai/springdemo/mvc/HelloWorldController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloWorldController {
 
 	// need a controller method to show the initial HTML form
