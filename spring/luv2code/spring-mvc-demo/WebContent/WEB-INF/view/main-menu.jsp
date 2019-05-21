723b04a891c05d4dd0ef98eb77d9a60bcf9f60c9
unknown <houarizegai14@gmail.com>
2019-05-21 12:43:45 +0100

    Spring MVC - get inputs from form

--- spring/luv2code/spring-mvc-demo/WebContent/WEB-INF/view/main-menu.jsp
+++ spring/luv2code/spring-mvc-demo/WebContent/WEB-INF/view/main-menu.jsp
@@ -6,5 +6,6 @@
 </head>
 <body>
 	<h2>Spring MVC Demo - Home Page !</h2>
+	<a href="showForm">Show Student Form</a>
 </body>
 </html>
---
+++ spring/luv2code/spring-mvc-demo/src/com/houarizegai/springdemo/mvc/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.houarizegai.springdemo.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+	// need a controller method to show the initial HTML form
+	
+	@RequestMapping("/showForm")
+	public String showForm() {
+		return "helloworld-form";
+	}
+	
+	// need a controller method to process the HTML form
+	@RequestMapping("/processForm")
+	public String processForm() {
+		return "helloworld";
+	}
+}
