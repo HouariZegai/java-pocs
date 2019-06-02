25ae98721b94037ed47ed9c5bd7559715f734803
unknown <houarizegai14@gmail.com>
2019-06-02 19:24:25 +0100

    CRM Project - adding customer dao

---
+++ spring/luv2code/web-customer-tracker/src/main/java/com/houarizegai/customertracker/dao/CustomerDAO.java
@@ -0,0 +1,11 @@
+package com.houarizegai.customertracker.dao;
+
+import com.houarizegai.customertracker.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerDAO {
+
+    public List<Customer> getCustomers();
+
+}
---
+++ spring/luv2code/web-customer-tracker/src/main/java/com/houarizegai/customertracker/dao/CustomerDAOImpl.java
@@ -0,0 +1,34 @@
+package com.houarizegai.customertracker.dao;
+
+import com.houarizegai.customertracker.entity.Customer;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+
+    // need to inject the session factory
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    @Transactional
+    public List<Customer> getCustomers() {
+        // get the currsnt hibernate session
+        Session session = sessionFactory.getCurrentSession();
+
+        // create a query
+        Query<Customer> query = session.createQuery("from customer", Customer.class);
+
+        // execute query and get result list
+        List<Customer> customers = query.getResultList();
+
+        return customers;
+    }
+}
