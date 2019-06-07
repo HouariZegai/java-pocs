fa20ee29eb7a3242e13e4961b5aeb85e9985ee15
HouariZegai <houarizegai14@gmail.com>
2019-06-07 15:32:02 +0100

    CRM REST Project - making end-point for get customers

--- spring/luv2code/Projects/spring-crm-rest/src/main/java/com/houarizegai/springcrmrest/rest/CustomerRestController.java
+++ spring/luv2code/Projects/spring-crm-rest/src/main/java/com/houarizegai/springcrmrest/rest/CustomerRestController.java
@@ -1,13 +1,23 @@
-package com.houarizegai.springcrmrest.controller;
+package com.houarizegai.springcrmrest.rest;
 
+import com.houarizegai.springcrmrest.entity.Customer;
 import com.houarizegai.springcrmrest.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequestMapping("/api")
 public class CustomerRestController {
     @Autowired
     private CustomerService customerService;
+
+    @GetMapping("/customers")
+    private List<Customer> getCustomers() {
+        return customerService.getCustomers();
+    }
+
 }
