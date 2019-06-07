f3feb8059f560358516d1a89d5d6126f6016a9dd
HouariZegai <houarizegai14@gmail.com>
2019-06-07 16:14:00 +0100

    CRM REST Project - adding GET Customer by id with overview image

--- spring/luv2code/Projects/spring-crm-rest/src/main/java/com/houarizegai/springcrmrest/rest/CustomerRestController.java
+++ spring/luv2code/Projects/spring-crm-rest/src/main/java/com/houarizegai/springcrmrest/rest/CustomerRestController.java
@@ -4,6 +4,7 @@ import com.houarizegai.springcrmrest.entity.Customer;
 import com.houarizegai.springcrmrest.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +17,14 @@ public class CustomerRestController {
     private CustomerService customerService;
 
     @GetMapping("/customers")
-    private List<Customer> getCustomers() {
+    public List<Customer> getCustomers() {
         return customerService.getCustomers();
+    }
+
+    @GetMapping("/customers/{customerId}")
+    public Customer getCustomer(@PathVariable int customerId) {
+        Customer customer = customerService.getCustomer(customerId);
+        return customer;
     }
 
 }
