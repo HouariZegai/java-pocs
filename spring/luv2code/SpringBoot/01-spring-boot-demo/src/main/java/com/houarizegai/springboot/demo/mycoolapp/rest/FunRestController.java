2311300a112bdd75ee40b2fe11ac1541589f3d5f
HouariZegai <houarizegai14@gmail.com>
2019-07-29 23:05:22 +0100

    SpringBoot - work with application.properties file

--- spring/luv2code/SpringBoot/01-spring-boot-demo/src/main/java/com/houarizegai/springboot/demo/mycoolapp/rest/FunRestController.java
+++ spring/luv2code/SpringBoot/01-spring-boot-demo/src/main/java/com/houarizegai/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,5 +1,6 @@
 package com.houarizegai.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,12 @@ import java.time.LocalDateTime;
 @RestController
 public class FunRestController {
 
+    @Value("${teacher.name}")
+    private String teacherName;
+
     // expose "/" that return "Hello World!"
     @GetMapping("/")
     public String sayHello() {
-        return "Hello World!, Time on the server is: " + LocalDateTime.now();
+        return "Hello World!, Teacher: " + teacherName + ", Time on the server is: " + LocalDateTime.now();
     }
 }
