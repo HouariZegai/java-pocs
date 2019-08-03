4e3fb2af66ef1e58fc035d2c84d75808029e04f5
HouariZegai <houarizegai14@gmail.com>
2019-08-03 14:40:45 +0100

    Web API - adding properties (customize end-points) to Spring Data REST

--- spring/luv2code/Projects/24-spring-boot-crud-spring-data-rest/src/main/java/com/houarizegai/springboot/cruddemo/dao/EmployeeRepository.java
+++ spring/luv2code/Projects/24-spring-boot-crud-spring-data-rest/src/main/java/com/houarizegai/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.houarizegai.springboot.cruddemo.dao;
 
 import com.houarizegai.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-// <table entity, type of primary key>
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path="members") // change the default path (this example:  employees) to "members"
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> { // <table entity, type of primary key>
     // that's it ... no need to write any code!
 }
