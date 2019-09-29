1f593183923e60fe06fdaff1c0aeaa6aa69eebce
HouariZegai <houarizegai14@gmail.com>
2019-09-29 23:30:49 +0100

    Junit - basics - run first test

---
+++ testing/JUnit/junit-5-basics/src/main/java/com/houarizegai/MathUtils.java
@@ -0,0 +1,9 @@
+package com.houarizegai;
+
+public class MathUtils {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+}
---
+++ testing/JUnit/junit-5-basics/src/test/java/com/houarizegai/MathUtilsTest.java
@@ -0,0 +1,11 @@
+package com.houarizegai;
+
+import org.junit.jupiter.api.Test;
+
+public class MathUtilsTest {
+
+    @Test
+    void test() {
+        System.out.println("This test run");
+    }
+}
