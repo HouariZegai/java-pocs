6690178dad126fd4a1ce317e5a32e672755da528
HouariZegai <houarizegai14@gmail.com>
2019-12-25 18:44:05 +0100

    Java 8 - working with Math API

---
+++ javase/src/com/houarizegai/javase/java8/MathDemo.java
@@ -0,0 +1,17 @@
+package com.houarizegai.javase.java8;
+
+public class MathDemo {
+
+    public static void main(String[] args) {
+        System.out.println(Math.pow(2, 3));
+        System.out.println(Math.ceil(2.2));
+        System.out.println(Math.floor(2.7));
+        System.out.println(Math.round(2.5));
+        System.out.println(Math.rint(12.51));
+        System.out.println(Math.abs(-10));
+        System.out.println(Math.sqrt(10));
+        System.out.println(Math.max(10d, 12.3));
+        System.out.println(Math.min(10d, 12.3));
+        System.out.println(Math.random());
+    }
+}
