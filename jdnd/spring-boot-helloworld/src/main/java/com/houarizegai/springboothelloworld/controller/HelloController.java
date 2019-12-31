70ad0148b9412e97a85993277b0975cdb5015db6
HouariZegai <houarizegai14@gmail.com>
2019-12-31 15:15:47 +0100

    JDND - spring boot - replace annotations (Controller & ResponseBody > RestController)

--- jdnd/spring-boot-helloworld/src/main/java/com/houarizegai/springboothelloworld/controller/HelloController.java
+++ jdnd/spring-boot-helloworld/src/main/java/com/houarizegai/springboothelloworld/controller/HelloController.java
@@ -1,18 +1,16 @@
 package com.houarizegai.springboothelloworld.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Controller
+@RestController
 public class HelloController {
     Map<String, Object> result = new HashMap<>();
 
     @RequestMapping("/hello")
-    @ResponseBody
     public Map<String, Object> hello() {
         result.put("name", "Houari");
         result.put("city", "Tiaret");
