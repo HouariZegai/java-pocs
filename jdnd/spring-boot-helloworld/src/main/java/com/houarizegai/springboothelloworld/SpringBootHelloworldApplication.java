5e9eea28c274197e959778b109554946caa927c6
HouariZegai <houarizegai14@gmail.com>
2019-12-31 15:41:22 +0100

    JDND - spring boot - spring boot - hello servlet

--- jdnd/spring-boot-helloworld/src/main/java/com/houarizegai/springboothelloworld/SpringBootHelloworldApplication.java
+++ jdnd/spring-boot-helloworld/src/main/java/com/houarizegai/springboothelloworld/SpringBootHelloworldApplication.java
@@ -2,8 +2,10 @@ package com.houarizegai.springboothelloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class SpringBootHelloworldApplication {
 
 	public static void main(String[] args) {
---
+++ jdnd/spring-boot-helloworld/src/main/java/com/houarizegai/springboothelloworld/servlet/HelloServlet.java
@@ -0,0 +1,19 @@
+package com.houarizegai.springboothelloworld.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name="helloServlet", urlPatterns = "/helloServlet")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.println("Running Hello Servlet doGet Method.");
+    }
+}
