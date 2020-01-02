b14ccdf5eb943684778a6820128644b0d97ec8db
HouariZegai <houarizegai14@gmail.com>
2020-01-02 01:31:58 +0100

    JDND - Thymeleaf - working with each (loop)

--- jdnd/spring-boot-thymeleaf/src/main/java/com/houarizegai/springbootthymeleaf/controller/UserController.java
+++ jdnd/spring-boot-thymeleaf/src/main/java/com/houarizegai/springbootthymeleaf/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.houarizegai.springbootthymeleaf.controller;
 
+import com.houarizegai.springbootthymeleaf.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -10,7 +14,34 @@ public class UserController {
     @RequestMapping("/demo")
     public String demo(Model model) {
         model.addAttribute("msg", "Hello Thymeleaf!");
+        double grade = 90.5;
+        model.addAttribute("grade", grade);
+        model.addAttribute("GPA", convertGPA(grade));
 
         return "demo";
+    }
+
+    @RequestMapping("/demo2")
+    public String demo1(Model model) {
+        List<User> lst = new ArrayList<>();
+        lst.add(new User(1, "Tom", 30));
+        lst.add(new User(2, "Jerry", 29));
+        lst.add(new User(3, "Nancy", 27));
+        model.addAttribute("list", lst);
+
+        return "demo2";
+    }
+
+    private String convertGPA(double grade) {
+        if(grade > 90)
+            return "A";
+        else if (grade < 90 && grade >= 80)
+            return "B";
+        else if (grade < 80 && grade >= 70)
+            return "C";
+        else if (grade < 70 && grade >= 60)
+            return "D";
+        else
+            return "F";
     }
 }
