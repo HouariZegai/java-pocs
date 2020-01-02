537f48c37241f93dcc2834e9791a119067015cc1
HouariZegai <houarizegai14@gmail.com>
2020-01-02 02:10:31 +0100

    JDND - Thymeleaf - working with URL

---
+++ jdnd/spring-boot-unit-tests/src/main/java/com/houarizegai/springbootunittests/SpringBootUnitTestsApplication.java
@@ -0,0 +1,13 @@
+package com.houarizegai.springbootunittests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUnitTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUnitTestsApplication.class, args);
+	}
+
+}
