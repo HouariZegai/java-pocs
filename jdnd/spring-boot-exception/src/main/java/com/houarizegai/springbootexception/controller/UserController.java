c4cf9a2b2197877c9d8282d8850eb7dd9455146d
HouariZegai <houarizegai14@gmail.com>
2020-01-03 00:31:38 +0100

    JDND - Spring Boot - Exception Hadling - handle each exception type to their error page

--- jdnd/spring-boot-exception/src/main/java/com/houarizegai/springbootexception/controller/UserController.java
+++ jdnd/spring-boot-exception/src/main/java/com/houarizegai/springbootexception/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.houarizegai.springbootexception.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
@@ -10,5 +12,29 @@ public class UserController {
     public String add() {
         int num = 10 / 0;
         return "add";
+    }
+
+    @RequestMapping("/update")
+    public String update() {
+        String name = null;
+        name = name.toLowerCase(); // this should cause null pointer exception
+        return "update";
+    }
+
+    @ExceptionHandler(value = {java.lang.ArithmeticException.class})
+    public ModelAndView handlerArithmeticException(Exception e) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("exception", e.toString());
+        modelAndView.setViewName("mathError");
+
+        return modelAndView;
+    }
+
+    @ExceptionHandler(value = {java.lang.NullPointerException.class})
+    public ModelAndView handleNullPointerException(Exception e) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("exception", String.valueOf(e));
+        modelAndView.setViewName("nullPointerError");
+        return modelAndView;
     }
 }
