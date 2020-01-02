79d88d0eab6f65e129be12cdaf69d665087db390
HouariZegai <houarizegai14@gmail.com>
2020-01-02 01:31:58 +0100

    JDND - Thymeleaf - working with Condition (if/switch)

--- jdnd/spring-boot-thymeleaf/src/main/java/com/houarizegai/springbootthymeleaf/controller/UserController.java
+++ jdnd/spring-boot-thymeleaf/src/main/java/com/houarizegai/springbootthymeleaf/controller/UserController.java
@@ -10,7 +10,23 @@ public class UserController {
     @RequestMapping("/demo")
     public String demo(Model model) {
         model.addAttribute("msg", "Hello Thymeleaf!");
+        double grade = 90.5;
+        model.addAttribute("grade", grade);
+        model.addAttribute("GPA", convertGPA(grade));
 
         return "demo";
+    }
+
+    private String convertGPA(double grade) {
+        if(grade > 90)
+            return "A";
+        else if (grade < 90 && grade >= 80)
+            return "B";
+        else if (grade < 80 && grade >= 70)
+            return "C";
+        else if (grade < 70 && grade >= 60)
+            return "D";
+        else
+            return "F";
     }
 }
