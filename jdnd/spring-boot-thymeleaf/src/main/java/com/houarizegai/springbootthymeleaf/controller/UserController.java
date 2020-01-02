9648a536b06b057a319bbc4ba27a8f4d00dee079
HouariZegai <houarizegai14@gmail.com>
2020-01-02 02:00:41 +0100

    JDND - Thymeleaf - working with Scope (request, session & application)

--- jdnd/spring-boot-thymeleaf/src/main/java/com/houarizegai/springbootthymeleaf/controller/UserController.java
+++ jdnd/spring-boot-thymeleaf/src/main/java/com/houarizegai/springbootthymeleaf/controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,12 +23,24 @@ public class UserController {
     }
 
     @RequestMapping("/demo2")
-    public String demo1(Model model) {
+    public String demo2(Model model) {
         List<User> lst = new ArrayList<>();
         lst.add(new User(1, "Tom", 30));
         lst.add(new User(2, "Jerry", 29));
         lst.add(new User(3, "Nancy", 27));
         model.addAttribute("list", lst);
+
+        return "demo2";
+    }
+
+    @RequestMapping("/demo3")
+    public String demo13(HttpServletRequest request, Model model) {
+        // Request
+        request.setAttribute("request", "request data");
+        // Session
+        request.getSession().setAttribute("session", "session data");
+        // Application
+        request.getSession().getServletContext().setAttribute("application", "application data");
 
         return "demo2";
     }
