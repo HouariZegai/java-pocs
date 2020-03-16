67f4323766187386c1b04a8e0f29b7e81748d4c4
HouariZegai <houarizegai14@gmail.com>
2020-03-16 11:10:33 +0100

    MicroServices & Spring Cloud - Making Configuration Service

---
+++ microservices-and-spring-cloud/service-config/src/test/java/com/houarizegai/serviceconfig/ServiceConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.houarizegai.serviceconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
