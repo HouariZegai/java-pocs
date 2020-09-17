56158191d11571a6483aa2ab39ac0040b8fbefda
HouariZegai <houarizegai14@gmail.com>
2020-09-17 23:28:51 +0100

    JDBC - PostgreSQL - made the Employee model & her dao interface

---
+++ jdbc/postgresql-demo/src/main/java/com/houarizegai/postgresqldemo/domain/Employee.java
@@ -0,0 +1,14 @@
+package com.houarizegai.postgresqldemo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+    private int id;
+    private String name;
+    private double salary;
+}
