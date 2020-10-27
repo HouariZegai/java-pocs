89a53bfa46c2fc595a5baff8ffe39ced7143cd64
HouariZegai <houarizegai14@gmail.com>
2020-10-27 23:52:41 +0100

    Functional - functional interface - worked with Supplier

--- functional/src/main/java/functionalinterface/_Supplier.java
+++ functional/src/main/java/functionalinterface/_Supplier.java
@@ -1,7 +1,18 @@
 package functionalinterface;
 
+import java.util.function.Supplier;
+
 public class _Supplier {
     public static void main(String[] args) {
-        
+        System.out.println(getDBConnectionUrl());
+
+        // Using supplier
+        System.out.println(getDBConnectionUrlSupplier.get());
+    }
+
+    static Supplier<String> getDBConnectionUrlSupplier = () -> "jdbc://localhost:3306/my_db";
+
+    static String getDBConnectionUrl() {
+        return "jdbc://localhost:3306/my_db";
     }
 }
