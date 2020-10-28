0aad0d5f18aa9dfbd44b1ec5841c854d460355ef
HouariZegai <houarizegai14@gmail.com>
2020-10-28 09:50:19 +0100

    Functional - working with callbacks

---
+++ functional/src/main/java/callbacks/Main.java
@@ -0,0 +1,37 @@
+package callbacks;
+
+import java.util.function.Consumer;
+
+public class Main {
+    public static void main(String[] args) {
+        hello("Houari", null, value -> System.out.println("No last name provided for " + value));
+        hello2("Houari", null, () -> System.out.println("No last name provided!"));
+    }
+
+    static void hello(String firstName, String lastName, Consumer<String> callback) {
+        System.out.println(firstName);
+        if(lastName != null) {
+            System.out.println(lastName);
+        } else {
+            callback.accept(firstName);
+        }
+    }
+
+    static void hello2(String firstName, String lastName, Runnable callback) {
+        System.out.println(firstName);
+        if(lastName != null) {
+            System.out.println(lastName);
+        } else {
+            callback.run();
+        }
+    }
+
+//    function hello(firstName, lastName, callback()) {
+//            console.log(firstName)
+//        if(lastName) {
+//            console.log(lastName)
+//        } else {
+//            callback()
+//        }
+//    }
+}
