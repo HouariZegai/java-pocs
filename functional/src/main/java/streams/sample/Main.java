35e3ed409e797a2c815f2cd9146c5002020e44de
HouariZegai <houarizegai14@gmail.com>
2020-10-29 13:55:01 +0100

    Functional - stream - worked with Max/Min

--- functional/src/main/java/streams/sample/Main.java
+++ functional/src/main/java/streams/sample/Main.java
@@ -2,6 +2,7 @@ package streams.sample;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -21,8 +22,8 @@ public class Main {
                 .sorted(Comparator.comparing(Person::getAge).reversed().thenComparing(Person::getGender))
                 .forEach(System.out::println);
 
-        // All match
-        System.out.println("// All match");
+        // Match
+        System.out.println("// Match");
         boolean allMatch = people.stream()
                 .allMatch(person -> person.getAge() > 18);
         System.out.println("All have age > 18? " + allMatch);
@@ -35,6 +36,15 @@ public class Main {
                 .noneMatch(person -> person.getName().equalsIgnoreCase("houari"));
         System.out.println("Houari doesn't exists on the list? " + noneMatch);
 
+        // Max / Min
+        System.out.println("// Max / Min");
+        Optional<Person> oldestPerson = people.stream()
+                .max(Comparator.comparing(Person::getAge));
+        oldestPerson.ifPresent(person -> System.out.println("Oldest one is: " + person));
+
+        people.stream()
+                .min(Comparator.comparing(Person::getAge))
+                .ifPresent(person -> System.out.println("Younger person is: " + person));
     }
 
     private static List<Person> getPeople() {
