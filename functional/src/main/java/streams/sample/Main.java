cea1c6c15bc334f561de4413774c40f611f3316f
HouariZegai <houarizegai14@gmail.com>
2020-10-29 13:33:51 +0100

    Functional - stream - worked with "all match"

--- functional/src/main/java/streams/sample/Main.java
+++ functional/src/main/java/streams/sample/Main.java
@@ -1,5 +1,6 @@
 package streams.sample;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,18 +9,27 @@ public class Main {
         List<Person> people = getPeople();
 
         // Filter
+        System.out.println("// Filtered");
         List<Person> females = people.stream()
                 .filter(person -> Gender.FEMALE.equals(person.getGender()))
                 .collect(Collectors.toList());
+
         females.forEach(System.out::println);
+
+        // Sort
+        System.out.println("// Sorted");
+        people.stream()
+                .sorted(Comparator.comparing(Person::getAge).reversed().thenComparing(Person::getGender))
+                .forEach(System.out::println);
+        
     }
 
     private static List<Person> getPeople() {
         return List.of(
                 new Person("Houari", 24, Gender.MALE),
                 new Person("Fatima", 50, Gender.FEMALE),
-                new Person("Mohamed", 39, Gender.MALE),
-                new Person("Aisha", 10, Gender.FEMALE)
+                new Person("Mohamed", 18, Gender.MALE),
+                new Person("Aisha", 24, Gender.FEMALE)
         );
     }
 }
