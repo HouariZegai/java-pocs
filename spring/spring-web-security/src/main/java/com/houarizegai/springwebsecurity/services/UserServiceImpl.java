760075f2e252ffe5005f7236bc18d5396ca112fb
HouariZegai <houarizegai14@gmail.com>
2020-11-03 18:17:24 +0100

    Spring Web security - Made User service Impl

---
+++ spring/spring-web-security/src/main/java/com/houarizegai/springwebsecurity/services/UserServiceImpl.java
@@ -0,0 +1,55 @@
+package com.houarizegai.springwebsecurity.services;
+
+import com.houarizegai.springwebsecurity.model.User;
+import com.houarizegai.springwebsecurity.repository.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private UserDao userDao;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public UserServiceImpl(UserDao userDao, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userDao = userDao;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userDao.findAll();
+    }
+
+    @Override
+    public Optional<User> findById(int id) {
+        return userDao.findById(id);
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return userDao.findByEmail(email);
+    }
+
+    @Override
+    public void save(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        userDao.save(user);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        userDao.deleteById(id);
+    }
+
+}
