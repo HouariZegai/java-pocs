4d38384f37a6af30ea9d018b337d909fd9097235
HouariZegai <houarizegai14@gmail.com>
2020-11-08 18:23:04 +0100

    Spring Web security - Added Springboot Test class

---
+++ spring/spring-web-security/src/test/java/com/houarizegai/springwebsecurity/SpringWebSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.houarizegai.springwebsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
