e7d9e8e50930e44f7de87932de6556c66ff0c302
HouariZegai <houarizegai14@gmail.com>
2020-11-10 23:50:58 +0100

    Spring Web security - Added database queries

---
+++ spring/spring-web-security/database_queries.sql
@@ -0,0 +1,9 @@
+DROP DATABASE IF EXISTS spring_mvc_security_db;
+
+CREATE DATABASE spring_mvc_security_db;
+
+CREATE TABLE user (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	email VARCHAR(50),
+	password VARCHAR(50)
+);
