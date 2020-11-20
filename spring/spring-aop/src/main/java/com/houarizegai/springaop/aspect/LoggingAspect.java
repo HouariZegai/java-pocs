e668ebb51e6c7720935f82e84c167fb9453a8c2c
HouariZegai <houarizegai14@gmail.com>
2020-11-20 22:39:49 +0100

    Spring AOP - working with Pointcut expressions

--- spring/spring-aop/src/main/java/com/houarizegai/springaop/aspect/LoggingAspect.java
+++ spring/spring-aop/src/main/java/com/houarizegai/springaop/aspect/LoggingAspect.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class LoggingAspect {
-    @Before("execution(public void save())")
+//    @Before("execution(public void save())")
+//    @Before("execution(public * sav*())")
+    @Before("execution(public void com.houarizegai.springaop.dao.UserRepository.save())")
     public void beforeSaveUserAdvice() {
         System.out.println("Aspect - @Before save user advice");
     }
