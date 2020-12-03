92e84e20b70702f8fdcd75b8cb85c70a825301a8
HouariZegai <houarizegai14@gmail.com>
2020-12-03 10:25:52 +0100

    Spring AOP - working with @Around advice & added Logger instead of sout

---
+++ spring/luv2code/SpringAOP/8-spring-aop-zz-around/src/main/java/com/houarizegai/springaop/SpringAopApplication.java
@@ -0,0 +1,13 @@
+package com.houarizegai.springaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAopApplication.class, args);
+    }
+
+}
