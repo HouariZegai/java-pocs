23fbd4f64d61203da3d7ccee25a23d6591e91eb6
HouariZegai <houarizegai14@gmail.com>
2020-12-11 15:31:38 +0100

    Spring security - Basic auth - used the in memory user details

--- spring/spring-security/spring-security-basic-auth/src/main/java/com/houarizegai/springsecuritybasicauth/security/WebSecurityConfig.java
+++ spring/spring-security/spring-security-basic-auth/src/main/java/com/houarizegai/springsecuritybasicauth/security/WebSecurityConfig.java
@@ -1,11 +1,27 @@
 package com.houarizegai.springsecuritybasicauth.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public WebSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -16,5 +32,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails houariUser = User.builder()
+                .username("houari")
+                .password(passwordEncoder.encode("root"))
+                .roles("STUDENT")
+                .build();
+        return new InMemoryUserDetailsManager(houariUser);
     }
 }
