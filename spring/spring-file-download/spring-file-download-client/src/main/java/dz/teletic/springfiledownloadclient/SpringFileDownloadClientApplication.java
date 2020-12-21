0c3db865e65fe42fe017997a816bffb7a7ec65c9
HouariZegai <houarizegai14@gmail.com>
2020-12-21 15:27:02 +0100

    Spring - download REST client - init the project

---
+++ spring/spring-file-download/spring-file-download-client/src/main/java/dz/teletic/springfiledownloadclient/SpringFileDownloadClientApplication.java
@@ -0,0 +1,13 @@
+package dz.teletic.springfiledownloadclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFileDownloadClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFileDownloadClientApplication.class, args);
+    }
+
+}
