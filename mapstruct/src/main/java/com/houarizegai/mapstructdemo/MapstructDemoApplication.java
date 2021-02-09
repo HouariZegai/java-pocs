7205e2e502b902f26f6e18812b408a3db95bec1d
HouariZegai <HouariZegai14@gmail.com>
2021-02-10 00:23:40 +0100

    MapStruct - using mapstruct to auto mapping the dto

---
+++ mapstruct/src/main/java/com/houarizegai/mapstructdemo/MapstructDemoApplication.java
@@ -0,0 +1,13 @@
+package com.houarizegai.mapstructdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapstructDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapstructDemoApplication.class, args);
+    }
+
+}
