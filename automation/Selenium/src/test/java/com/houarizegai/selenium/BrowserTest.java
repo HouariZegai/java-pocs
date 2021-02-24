d774d59268c99e2b538bfb3e87486d90c71c9213
HouariZegai <HouariZegai14@gmail.com>
2021-02-24 14:43:52 +0100

    Selenium - dynamic get the chrome driver

--- automation/Selenium/src/test/java/com/houarizegai/selenium/BrowserTest.java
+++ automation/Selenium/src/test/java/com/houarizegai/selenium/BrowserTest.java
@@ -9,7 +9,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class BrowserTest {
 
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "D:/learning/JavaLearning/automation/Selenium/src/main/resources/drivers/chromedriver.exe");
+        String projectPath = System.getProperty("user.dir");
+
+        System.setProperty("webdriver.chrome.driver", projectPath + "/src/main/resources/drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://google.com");
     }
