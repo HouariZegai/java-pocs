7e2d5b750b951cd0b5ef16bf733fb2c2dbbe9f73
HouariZegai <houarizegai14@gmail.com>
2021-07-30 00:40:28 +0100

    Spring mongo - use indexted and Query criteria

--- spring/spring-mongo-crud/src/main/java/com/houarizegai/springmongocrud/SpringMongoCrudApplication.java
+++ spring/spring-mongo-crud/src/main/java/com/houarizegai/springmongocrud/SpringMongoCrudApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -17,15 +20,27 @@ public class SpringMongoCrudApplication {
     }
 
     @Bean
-    CommandLineRunner runner(StudentRepository studentRepository) {
+    CommandLineRunner runner(StudentRepository studentRepository, MongoTemplate mongoTemplate) {
         return args -> {
             Address address = new Address("Algeria", "Tiaret", "14000");
 
             List<String> favouriteSubjects = List.of("Software Engineering", "Java", "Spring framework");
-            Student student = new Student("Houari", "Zegai", "houarizegai14@gmail.com",
+            String email = "gwwkhouari@gmail.com";
+            Student student = new Student("Houari", "Zegai", email,
                     Gender.MALE, address, favouriteSubjects, BigDecimal.TEN, LocalDateTime.now());
 
-            studentRepository.insert(student);
+            Query query = new Query();
+            query.addCriteria(Criteria.where("email").is(email));
+            List<Student> students = mongoTemplate.find(query, Student.class);
+            if (students.size() > 1) {
+                throw new IllegalStateException("Found many students with email " + email);
+            }
+
+            if (students.isEmpty()) {
+                studentRepository.insert(student);
+            } else {
+                System.out.println("Student already exist!");
+            }
         };
     }
 }
--- spring/spring-mongo-crud/src/main/java/com/houarizegai/springmongocrud/Student.java
+++ spring/spring-mongo-crud/src/main/java/com/houarizegai/springmongocrud/Student.java
@@ -2,6 +2,7 @@ package com.houarizegai.springmongocrud;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
@@ -16,6 +17,7 @@ public class Student {
     private String id;
     private String firstName;
     private String lastName;
+    @Indexed(unique = true)
     private String email;
     private Gender gender;
     private Address address;
