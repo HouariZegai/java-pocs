01220a89a12182912e75b07eaddd516db0ba9ef6
HouariZegai <houarizegai14@gmail.com>
2021-09-09 16:09:23 +0100

    Spring minio - init project

---
+++ spring/spring-minio/src/main/java/com/houarizegai/springminio/SpringMinioApplication.java
@@ -0,0 +1,13 @@
+package com.houarizegai.springminio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMinioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMinioApplication.class, args);
+	}
+
+}
---
+++ spring/spring-minio/src/test/java/com/houarizegai/springminio/SpringMinioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.houarizegai.springminio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMinioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
