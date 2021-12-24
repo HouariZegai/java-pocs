e2dfb841a0964b7b0dc674d625821cdf783fe342
houarizegai <houarizegai14@gmail.com>
2021-12-24 17:28:30 +0100

    oca - ch5: added static hiding methods sample

---
+++ oca/src/main/java/com/houarizegai/oca/ch5/interfaces/Marsupial.java
@@ -0,0 +1,29 @@
+package com.houarizegai.oca.ch5.interfaces;
+
+class Marsupial {
+
+    public static boolean isBiped() {
+        return false;
+    }
+
+    public void getMarsupialDescription() {
+        System.out.println("Marsupial walks in twi legs: " + isBiped());
+    }
+}
+
+class Kangaroo extends Marsupial {
+
+    public static boolean isBiped() {
+        return true;
+    }
+
+    public void getKangarooDescription() {
+        System.out.println("Kangaroo hops on two legs: " + isBiped());
+    }
+
+    public static void main(String[] args) {
+        Kangaroo joey = new Kangaroo();
+        joey.getMarsupialDescription();
+        joey.getKangarooDescription();
+    }
+}
