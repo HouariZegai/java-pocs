764a05ae2e34bf96a8381638831c2828c5b7671e
houarizegai <houarizegai14@gmail.com>
2021-12-29 21:49:18 +0100

    feat: oca - test static final function

---
+++ oca/src/main/java/com/houarizegai/oca/ch4/FinalDemo.java
@@ -0,0 +1,12 @@
+package com.houarizegai.oca.ch4;
+
+public class FinalDemo {
+
+    final static public void sayHello() {
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args) {
+        sayHello();
+    }
+}
