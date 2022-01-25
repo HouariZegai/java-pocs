91d4401fedcd7a96596a7df5aa2887c4a2d1164f
houarizegai <houarizegai14@gmail.com>
2022-01-25 22:46:15 +0100

    oca - added sum question

---
+++ oca/src/main/java/com/houarizegai/oca/exam/Q6.java
@@ -0,0 +1,10 @@
+package com.houarizegai.oca.exam;
+
+public class Q6 {
+
+    public static void main(String[] args) {
+        int x = 9;
+        long y = x * (long) (++x);
+        System.out.println(y);
+    }
+}
