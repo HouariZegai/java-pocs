62808d9de9c9cb074fd84f7e0b3dfe4a247e2f1c
houarizegai <houarizegai14@gmail.com>
2022-01-25 22:50:00 +0100

    oca - added question 56

---
+++ oca/src/main/java/com/houarizegai/oca/exam/Q56.java
@@ -0,0 +1,12 @@
+package com.houarizegai.oca.exam;
+
+public class Q56 {
+
+    public static void main(String[] args) {
+        String tiger = "Tiger";
+        String lion = "Lion";
+        // line below does not compile
+        //final String statement = 250 > 338 ? lion : tiger = " is Bigger";
+        System.out.println(statement);
+    }
+}
