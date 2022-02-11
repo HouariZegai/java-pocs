deb009ab2e3a324f9c538533015cdf426a50b44a
houarizegai <houarizegai14@gmail.com>
2022-02-11 12:54:27 +0100

    feat: oca - made substring demo

---
+++ oca/src/main/java/com/houarizegai/oca/SubstringDemo.java
@@ -0,0 +1,12 @@
+package com.houarizegai.oca;
+
+public class SubstringDemo {
+
+    public static void main(String[] args) {
+        String s = new String("JavaSE8Certification");
+        s = s.substring(2, 10);
+        s = s.substring(2, 7);
+        s = s.substring(2, 5);
+        System.out.println(s);
+    }
+}
